97c1df278a232482387cefedee1564908f208888
Josué <josuefonseca17@outlook.com>
2024-04-23 11:40:06 -0300

    Adicionando sons0

--- index.js
+++ index.js
@@ -18,10 +18,16 @@ if(clrOrder == "red"){
     song.play();
 } else if(clrOrder == "green"){
     $("#sqrG").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var song = $("#songG")[0];
+    song.play();
 } else if(clrOrder == "blue"){
     $("#sqrB").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var song = $("#songB")[0];
+    song.play();
 } else if(clrOrder == "yellow"){
     $("#sqrY").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var song = $("#songY")[0];
+    song.play();
 }
 
 console.log(clrOrder);
