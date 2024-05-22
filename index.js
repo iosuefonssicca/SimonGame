bcd823a02ce52abfdba57e06d7977a76aac6bf68
Josué <josuefonseca17@outlook.com>
2024-05-22 11:35:41 -0300

    creatinglevels

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
 var colors = ["red","green","blue","yellow"];
 var nmbrRandom = [];
 var clrOrder = [];
-var numberOfSqrs = document.querySelectorAll(".sqr").length;
 var usrSequence = [];
+var gameSequence = [];
+var started = false;
+var level = 0;
 
+$(document).keypress(function() {
+    if (!started) {
+      $("#ttl0").text("Level " + level);
+      rdmSequence();
+      started = true;
+    }
+  });
+  
 
-function rdmSequence(idSqr){
+$(".sqr").click(function (){
+    var usrClickedColor = $(this).attr("id");
+    usrSequence.push(usrClickedColor);
+    userClick(usrClickedColor);
+});
+
+function rdmSequence(){
+
+level++;
+$("#ttl0").text("Level " + level);
 
 nmbrRandom = Math.random();
 nmbrRandom = nmbrRandom * 4;
 nmbrRandom = Math.floor(nmbrRandom);
 
-
 clrOrder.push(colors[nmbrRandom]);
 
-console.log(idSqr);
-
-if(clrOrder == "red" || idSqr == "sqrR" ){
+if(clrOrder == "red"){
     $("#sqrR").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var song = $("#songR")[0];
     song.play();
-} else if(clrOrder == "green" || idSqr == "sqrG"){
+    gameSequence.push("sqrR");
+} else if(clrOrder == "green"){
     $("#sqrG").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var song = $("#songG")[0];
     song.play();
-} else if(clrOrder == "blue" || idSqr == "sqrB"){
+    gameSequence.push("sqrG");
+} else if(clrOrder == "blue"){
     $("#sqrB").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var song = $("#songB")[0];
     song.play();
-} else if(clrOrder == "yellow" || idSqr == "sqrY"){
+    gameSequence.push("sqrB");
+} else if(clrOrder == "yellow"){
     $("#sqrY").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var song = $("#songY")[0];
     song.play();
+    gameSequence.push("sqrY");
 }
 
+console.log(gameSequence);
+
 }
 
-
-// for(var i = 0; i<numberOfSqrs;i++){
-//     console.log(numberOfSqrs);
-//     var id = [];
-//     id = this.id;
-//     document.querySelectorAll(".sqr")[i].addEventListener("click", sqrClick);
-// }
-
-
-$(".sqr").click(function test(){
-        var usrClickedColor = $(this).attr("id");
-        usrSequence.push(usrClickedColor);
-        console.log(usrClickedColor);
-    rdmSequence(usrClickedColor);
-    
-});
-
-
-
-function sqrClick(){
-    console.log();
+function userClick(idSqr){
+    if(idSqr == "sqrR" ){
+        $("#sqrR").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        var song = $("#songR")[0];
+        song.play();
+    } else if(idSqr == "sqrG"){
+        $("#sqrG").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        var song = $("#songG")[0];
+        song.play();
+    } else if(idSqr == "sqrB"){
+        $("#sqrB").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        var song = $("#songB")[0];
+        song.play();
+    } else if(idSqr == "sqrY"){
+        $("#sqrY").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        var song = $("#songY")[0];
+        song.play();
+    }
 }
+
 
 
 
