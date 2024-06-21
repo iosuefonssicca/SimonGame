b60b0ace15bd2591e3b244677d8a49d9b9c50570
Josué <josuefonseca17@outlook.com>
2024-06-21 11:19:48 -0300

    AddingSoundsandMovement

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ function nextSequence(){
 
     randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("./" + randomChosenColor + ".mp3");
+    audio.play();
+    
 }
+
+$(".sqr").fadeIn(100).fadeOut(100).fadeIn(100).click(function (){
+    var audio = new Audio("./" + randomChosenColor + ".mp3");
+    audio.play();
+});
 
 nextSequence();
 console.log(gamePattern);
