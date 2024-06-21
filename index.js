b482c5f784c2ee660d57db4fc31a6172ad2fe90d
Josué <josuefonseca17@outlook.com>
2024-06-21 14:46:55 -0300

    StartingTheGame

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var randomChosenColor;
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 1;
+
+$(document).keypress(function() {
+    if (!started) {
+      $("#ttl0").text("Level " + level);
+      rdmSequence();
+      started = true;
+    }
+});
 
 $(".sqr").click(function (){
     var userChosenColour = $(this).attr("id");
@@ -11,6 +21,8 @@ $(".sqr").click(function (){
     playSound(userChosenColour);
     animatedPress(userChosenColour);
 });
+
+
 
 function nextSequence(){
     
@@ -21,6 +33,8 @@ function nextSequence(){
     randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    level++;
 }
 
 function animatedPress(currentColor){
@@ -35,8 +49,3 @@ function playSound(name){
     var audio = new Audio("./" + name + ".mp3");
     audio.play();
 }
-
-nextSequence();
-console.log(gamePattern);
-
-
